a6e9ab5135364e156b88f5fbeffe5e14182c3870
Kasoma Ibrahim <kasomaibrahim@gmail.com>
2022-02-01 13:53:17 +0000

    JSON data

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const app = express();
-const data = require('./data.json')
+const {projects} = require('./data.json')
 
 app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
-// app.get('*', (req,res) => [
-//     res.send('sorry page not found')
-// ])
-
 app.get('/', (req, res) => {
-    res.render('index', {projects: data.projects})
+    res.render('index', {projects})
     
 })
 
@@ -18,14 +14,16 @@ app.get('/about', (req, res) => {
     res.render('about')
 })
 
+
 app.get('/project/:id', (req, res) => {
-    res.render('project', {id:req.params.id})
+    res.render('project', {id:req.params.id, projects})
 })
 
+// Error Handling for 404 status errors
 app.use((req,res,next) => {
     res.status(404).send('Sorry cant find that!')
 })
-
+//Error Handling for Global status errors
 app.use((err,req,res,next) => {
     res.status(500).send('Something is brocken please try again')
 })
